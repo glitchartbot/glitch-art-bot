19187cd59de6806a42b690b7784ddf47f7f7d97a
friaca <v.fria@outlook.com>
2020-03-03 00:11:42 -0300

    add: Comentários no `index`

--- src/index.js
+++ src/index.js
@@ -14,24 +14,27 @@ async function onMention(tweet) {
     const { data } = await bot.getTweetById(parentId);
 
     if (myUtil.hasValidImage(data) && tweet.in_reply_to_user_id_str != bot.ID) {
+      //Baixa a imagem do tweet
       const imageUrl = myUtil.getImageUrl(data);
       const fileFormat = myUtil.getFileFormat(data);
       const chosenSketch = SketchesEnum.pixelSort;
       await myUtil.downloadImage(imageUrl, chosenSketch, { fileName: tweetId, fileFormat });
 
+      //Salva as informações necessárias pra processar depois
       const sketchSetupPath = processing.getSetupFilePath(chosenSketch);
       myUtil.saveInfo(sketchSetupPath, `./assets/${tweetId}`, fileFormat);
       
+      //Executa o comando que edita a imagem
       const cmd = processing.getProcessingCmd(chosenSketch);
       const { stdout, stderr } = await execAsync(cmd);
       
+      //Responde o tweet que mencionou ele 
       const replyTweet = { 
         sketch: chosenSketch, 
         fileName: `${tweetId}_1${fileFormat}`
       }
       const reply = await bot.replyTweet(tweetId, replyTweet);
 
-      console.log('Sucesso!\n' + myUtil.getTweetUrl(reply));
       myUtil.log({status: 'success', tweet, parent: data});
     } else {
       console.log('Imagem inválida');
